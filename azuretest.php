a3c299f9c77987ba72c1a510a39187c1113f7536
piax93 <piax93@hotmail.it>
2015-08-21 10:38:19 +0200

    Update azuretest.php

--- azuretest.php
+++ azuretest.php
@@ -1,10 +1,2 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-</head>
-<body>
-<p>Ciao Azure, ti prego leggimi</p>
-<p><?php echo 'e stampa questa frase';?></p>
-</body>
-</html>
+Ciao Azure, ti prego leggimi
+<?php echo 'e stampa questa frase';?>
