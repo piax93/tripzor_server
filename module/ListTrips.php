f57e0c2b11aaf3e1d761490b0eb0ef780c6a6d49
mat <piax93@hotmail.it>
2015-08-29 10:53:38 +0200

    list trips

--- module/ListTrips.php
+++ module/ListTrips.php
@@ -5,7 +5,10 @@ if($user->selectByEmail($_SESSION['user'])){
 	$query = 'SELECT name FROM trip WHERE userId = "' . $user->getUserId() . '"';
 	$db = Database::getDbInstance();
 	$trips = $db->execQuery($query);
-	print_r($trips);
+	for($i = 0; $i < count($trips); $i++){
+		echo $trips[$i]['name'];
+		if($i != count($trips)-1) echo PHP_EOL;
+	}
 }else{
 	ReturnCode::$userNotFound;
 }
