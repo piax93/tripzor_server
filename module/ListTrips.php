10065448ec43e3071bc97874b92809dad844c236
mat <piax93@hotmail.it>
2015-08-29 11:02:04 +0200

    go2

--- module/ListTrips.php
+++ module/ListTrips.php
@@ -6,11 +6,7 @@ if($user->selectByEmail($_SESSION['user'])){
 	$db = Database::getDbInstance();
 	$trips = $db->execQuery($query);
 	for($i = 0; $i < count($trips); $i++){
-<<<<<<< HEAD
 		echo $trips[$i]['name'];
-=======
-		echo $trips[$i];
->>>>>>> 7658ff617d2add70b768ca9aa3a281ad72415dcc
 		if($i != count($trips)-1) echo PHP_EOL;
 	}
 }else{
