d68b3f00a4df55c594073a13791326096210644c
mat <piax93@hotmail.it>
2015-09-09 11:51:27 +0200

    mailfinalfix

---
+++ get_auth_token.php
@@ -0,0 +1,5 @@
+<?php 
+
+$redirectUri = 'https://tripzor.azurewebsites.net/get_auth_token.php';
+$clientId = '165494771375-mnd868a7b94637vbs1ip092j39nbk5bs.apps.googleusercontent.com';
+$clientSecret = 'suit-EuTnupgND0_ellh7_rI';
