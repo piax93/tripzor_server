16609090d389db0adb24f8ce623a1de108331aac
mat <piax93@hotmail.it>
2015-09-09 18:47:51 +0200

    mailfix

--- module/ForgottenPassword.php
+++ module/ForgottenPassword.php
@@ -1,5 +1,4 @@
 <?php
-require_once 'vendor/autoload.php';
 
 $_POST['email'] = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 $user = new User();
@@ -7,34 +6,14 @@ if($user->selectByEmail($_POST['email'])){
     $newPassword = uniqid();
     $user->setPassword(md5($newPassword));
     if($user->update()){
-    	include '/utilities/values/mailCredentials.php';
-    	$mail = new PHPMailer();    	
     	$body = "Dear " . $user->getName() . ",\r\n\r\n"
                 . "Your password has been reset.\r\n"
                 . "Your new password is: $newPassword.\r\n"
                 . "We suggest you to change your password to a more familiar one as soon as possible.\r\n\r\n"
                 . "Have a nice day.\r\n"
-                . "Tripzor Team";
-    	
-    	$mail->IsSMTP();
-    	$mail->ContentType = 'text/plain';
-    	$mail->IsHTML(false);
-    	$mail->SMTPDebug  = 1; // 1 = errors and messages 2 = messages only
-    	$mail->SMTPAuth   = true;                  
-    	$mail->SMTPSecure = "tls";    	
-    	$mail->Host       = gethostbyname('smtp.gmail.com');      // sets GMAIL as the SMTP server
-    	$mail->Port       = 587;   // set the SMTP port for the GMAIL server
-    	$mail->SMTPKeepAlive = true;
-    	$mail->Mailer = "smtp";
-    	$mail->Port       = $mail_port;                   
-    	$mail->Username   = $mail_user;
-    	$mail->Password   = $mail_password;
-    	$mail->Body = $body;
-    	$mail->SetFrom($mail_user, 'Tripzor');    	
-    	$mail->Subject    = 'Tripzor Password Reset';
-    	$mail->AddAddress($user->getEmail(), $user->getNickname());
-    	
-    	if(!$mail->Send()) {
+                . "Tripzor Team";    	
+    	$res = MailSender::sendMail($user->getEmail(), 'Tripzor Password Reset', $body);  	
+    	if(!$res) {
     		echo ReturnCode::$mailError;
     	} else {
     		echo ReturnCode::$success;
---
+++ utilities/MailSender.php
@@ -0,0 +1,20 @@
+<?php
+
+class MailSender {
+	
+	public static function sendMail($dest, $title, $body){
+		require_once 'vendor/autoload.php';
+		include 'values/mailCredentials.php';
+		
+		$sendgrid = new SendGrid($mail_user, $mail_password);
+		$email    = new SendGrid\Email();
+		
+		$email->addTo($dest)
+		->setFrom('noreply@tripzor.org')
+		->setSubject($title)
+		->setText($body);
+		
+		$sendgrid->send($email);		
+	}
+
+}
