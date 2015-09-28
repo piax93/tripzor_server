a507c88596a5a9c4c48cd90ce320ce68fbf6a349
piax93 <piax93@hotmail.it>
2015-09-28 16:15:44 +0200

    registration mail

--- module/UserRegistration.php
+++ module/UserRegistration.php
@@ -9,6 +9,9 @@ if($res !== false){
     $user->fillByAssoc($_POST);
     $res = $user->insert();
     if($res !== false){
+    	$body = 'Dear ' . $user->getName() . ',' . PHP_EOL . PHP_EOL .
+			'Thanks for registering to our service, we wish you a happy user experience.';
+    	MailSender::sendMail($_POST['email'], 'Welcome to Trizor', $body);
         echo ReturnCode::$success;
     }else{
         echo ReturnCode::$error;
