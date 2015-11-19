bdaa50da5a660cbc35335c4d2890f14647bbd216
piax93 <piax93@hotmail.it>
2015-11-19 22:39:56 +0100

    stuff

--- index.php
+++ index.php
@@ -6,17 +6,17 @@ include 'ClassLoader.php';
 // POST FILTERING
 foreach ($_POST as $key => $value) {
     $value = strip_tags($value);
-    // $_POST[$key] = preg_replace('/\s+/', '', $value);
+    // $value = preg_replace('/\s+/', '', $value);
+    $_POST[$key] = $value;
 }
 
 ClassLoader::loadAll();
-echo Database::sessionDecrypt('Tzptm2192nSJ1uJZV7ftuWl/Fmep2L6Zs+uxi0Ck6bo=');
 
 if(!empty($_POST)){
     if(isset($_POST['action'])){
         ModuleLoader::loadModule($_POST['action']);
     }
 }else{
-	//sleep(1);
+	sleep(1);
     echo 'SERVER UP AND RUNNING';
 }
