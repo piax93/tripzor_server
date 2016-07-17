c6cb49a6fc6cfa5f81c557878762785aed587a2c
piax93 <piax93@hotmail.it>
2016-07-17 23:08:09 +0200

    trip date check

--- module/AddTrip.php
+++ module/AddTrip.php
@@ -1,21 +1,26 @@
 <?php
-session_start();
-$user = new User();
-if($user->selectByEmail(Database::sessionDecrypt($_SESSION['user']))){
-    $trip = new Trip();
-    $trip->fillByAssoc($_POST);
-    $trip->setUserId($user->getUserId());
-    $trip->setNPart(1);
-    $res = $trip->insert();
-    if($res){
-    	Logger::var_dump_log('AddTrip', $trip->getName());
-        echo ReturnCode::$success;
-    }else{
-    	Logger::log('AddTrip', 'Failed');
-        echo ReturnCode::$error;
-    }
-}else{
-    echo ReturnCode::$userNotFound;
+session_start ();
+$user = new User ();
+if ($user->selectByEmail ( Database::sessionDecrypt ( $_SESSION ['user'] ) )) {
+	$trip = new Trip ();
+	$trip->fillByAssoc ( $_POST );
+	$trip->setUserId ( $user->getUserId () );
+	$trip->setNPart ( 1 );
+	
+	if (Util::compareDate ( $trip->getStartDate (), $trip->getEndDate () )) {
+		$res = $trip->insert ();
+		if ($res) {
+			Logger::var_dump_log ( 'AddTrip', $trip->getName () );
+			echo ReturnCode::$success;
+		} else {
+			Logger::log ( 'AddTrip', 'Failed' );
+			echo ReturnCode::$error;
+		}
+	} else {
+		echo ReturnCode::$error;
+	}
+} else {
+	echo ReturnCode::$userNotFound;
 }
 
 
